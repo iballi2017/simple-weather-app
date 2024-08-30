8b2e6cedf5d714bfaa8adb890421db239eb5534d
iballidev <iballi.dev@gmail.com>
2024-08-30 12:11:03 +0100

    working app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="device-screen">
+        {/* Next: to stile 'device-screen' as a device screen */}
         <Weather />
       </div>
     </main>
