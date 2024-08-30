d0b5f1e410c3375558cd1f2f10b68e9c6b390cc1
iballidev <iballi.dev@gmail.com>
2024-08-30 12:09:54 +0100

    working app

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Weather from "./components/weather";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="device-screen">
+        <Weather />
+      </div>
+    </main>
+  );
+}
