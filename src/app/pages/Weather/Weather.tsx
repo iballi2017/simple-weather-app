a9c0d6c2f7fac24177a6c2af1352f17f76bd94a8
iballidev <iballi.dev@gmail.com>
2024-08-31 12:41:47 +0100

    update

--- src/app/pages/Weather/Weather.tsx
+++ src/app/pages/Weather/Weather.tsx
@@ -54,7 +54,7 @@ const Weather = () => {
             {loading && <p>Loading...</p>}
             {error && <p className='text-red-400 text-sm'>{error}</p>}
             {weather && (
-                <div className='mt-8 mb-4 flex flex-col gap-2' data-testid="details-card">
+                <div className='mt-8 mb-4 flex flex-col gap-2' data-testid={"details-card"}>
                     <h2 className='font-semibold text-3xl'>{weather.name}, {weather.sys.country}.</h2>
                     <p className='text-xs text-gray-400'>{convert_date(weather.dt)}</p>
                     <div className='flex items-center'>
